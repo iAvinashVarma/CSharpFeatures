b0e70bc2d5d0af5f5de3eb96cd626ef81f6f9e83
iAvinashVarma <avinashvarma@live.com>
2019-02-12 21:22:43 +0800

    Re-adding C#5 test cases.

---
+++ CSharp5/CSharp5.Tests/AsyncWay/1PeopleTest.cs
@@ -0,0 +1,29 @@
+﻿using CSharp5.AsyncWay;
+using CSharp5.AsyncWay.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CSharp5.Tests.AsyncWay
+{
+	[TestClass]
+	public class _1PeopleTest
+	{
+		[TestMethod]
+		[ExpectedException(typeof(HttpRequestException))]
+		public async Task TestPeopleMethod001()
+		{
+			// Arrange
+			People people = new People();
+
+			// Act
+			IEnumerable<Person> persons = await people.GetPeopleAsync();
+
+			// Assert
+			Assert.IsNotNull(persons);
+			Assert.IsTrue(persons.Any());
+		}
+	}
+}
---
+++ CSharp5/CSharp5.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CSharp5.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CSharp5.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6b6a8b2d-37a6-487e-b257-5e8d622138f7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
